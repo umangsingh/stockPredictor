84be10d49815e9b22b903fcfb0dea48ae657a4d6
AgentT <AgentT@users.noreply.github.com>
2016-08-31 22:13:31 +0200

    Add files via upload

--- sharekhan/items.py
+++ sharekhan/items.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
 
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class SharekhanItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+     year = scrapy.Field()
+     eps  = scrapy.Field()    
+    
